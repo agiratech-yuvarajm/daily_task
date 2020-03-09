72a64b5f62e71672963813ee47f62a8bf9100de5
yuvaraj <yuvaraj@agiratech.com>
2020-03-09 19:53:50 +0530

    09/03/2020

---
+++ Category_Skill.sql
@@ -0,0 +1,56 @@
+insert into skill_categories (category_id, department_id, category_name, category_active) values (1, 1,'Languages',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (2, 1,'Frameworks',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (3, 1,'JS_Frameworks	',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (4, 1,'Storage/DB',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (5, 1,'CMS / eCommerce',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (6, 1,'DevOps',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (7, 1,'Dev_Tools',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (8, 1,'Testing_Tools',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (9, 1,'Mobile_Dev',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (10, 1,'Proj_Mgmt_Tools',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (11, 1,'Payment_Gateways',true);
+insert into skill_categories (category_id, department_id, category_name, category_active) values (12, 1,'Design_Tools',true);
+
+
+
+INSERT INTO skill_categories (category_id, department_id, category_name, category_active)
+VALUES  (1, 1,'Languages',true),
+        (2, 1,'Frameworks',true),
+        (3, 1,'JS_Frameworks',true),
+	(4, 1,'Storage/DB',true),
+	(5, 1,'CMS / eCommerce',true),
+	(6, 1,'DevOps',true),
+	(7, 1,'Dev_Tools',true),
+	(8, 1,'Testing_Tools',true),
+	(9, 1,'Mobile_Dev',true),
+	(10, 1,'Proj_Mgmt_Tools',true),
+	(11, 1,'Payment_Gateways',true),
+	(12, 1,'Design_Tools',true);
+	
+	
+	
+INSERT INTO skill_items (skill_id, category_id, skill_name, skill_active)
+VALUES  (1, 1,'{'PHP','Ruby','Python','GoLang','Java','C/C++','Typescript','Javascript','R Lang','.Net','HTML5','CSS3'}',true);
+        
+INSERT INTO skill_items (skill_id, category_id, skill_name, skill_active)
+VALUES (1, 1,'PHP',true);
+
+
+
+
+
+INSERT INTO ''table'' (''column1'' [, ''column2'', ''column3'' ... ]) VALUES (''value1'' [, ''value2'', ''value3'' ... ])
+
+
+
+INSERT INTO skill_items (category_id, department_id, [ category_name], skill_active) VALUES (1,1 ,[ ''PHP','Ruby''], true);
+
+
+
+insCmd.CommandText = "INSERT into Holidays (Holidays) values('" & _
+        txtHol1.Text & "','" & txtHol2.Text & "','" & txtHol3.Text & "','" & txtHol4.Text & "','" & txtHol5.Text & "','" & _
+        txtHol6.Text & "','" & txtHol7.Text & "','" & txtHol8.Text & "', '" & txtHol9.Text & "','" & _
+        txtHol10.Text & "','" & txtHol11.Text & "','" & txtHol12.Text & "','" & txtHol13.Text & "','" & txtHol14.Text & "','" _
+        & txtHol15.Text & "')"
+
+
